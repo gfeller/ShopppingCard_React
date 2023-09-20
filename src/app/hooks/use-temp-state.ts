39d08b1e5589a8009b43ebac46920bc9cc9fdc7d
Michael Gfeller <michael.gfeller@ost.ch>
2023-09-20 11:54:38 +0200

    small fix

--- src/app/hooks/use-temp-state.ts
+++ src/app/hooks/use-temp-state.ts
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 
-export function useTmpState<T>(initValue : T, timeout: number = 1000) {
+export function useTmpState<T>(initValue : T, timeout = 1000) {
     const [state, setState] = useState<T>(initValue);
 
     useEffect(() => {
